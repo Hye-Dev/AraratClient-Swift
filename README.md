e74693254cfdbcb35b52e21d09e110bf872699b2
Joseph Maldjian <josephavm@icloud.com>
2022-05-26 09:44:12 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AraratClient
+
+A description of this package.
---
+++ Tests/AraratClientTests/AraratClientTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AraratClient
+
+final class AraratClientTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AraratClient().text, "Hello, World!")
+    }
+}
